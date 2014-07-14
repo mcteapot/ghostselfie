fff5de6d92037d710aa83066edee1142443f6a3b
Arjun Prakash <archieoi@gmail.com>
2014-07-14 03:15:26 -0700

    Synchronous vs Asynchronous code with Node.js
    http://stackoverflow.com/questions/13858909/synchronous-vs-asynchronous-code-with-node-js

--- index.js
+++ index.js
@@ -10,17 +10,26 @@ var logwriter = require('./lib/logwriter')
 // Inits
 var Twit = new twit(config1);
 
-var LogWriter = new logwriter('ghostLog.txt');
+var LogWriter = new logwriter('ghostLog.txt', function(exists) {
 
-console.log(config1);
 
-/*
-console.log("TRACKING #therecameanecho")
+});
+
+//console.log(config1);
+
+
+//LogWriter.writeUserAndTime('mikel jordan','funkeybutt', 'Fri Jul 11 20:21:26 +0000 2014');
+//LogWriter.writeUserAndTime('rank lombard','rockandroller', 'Fri Jul 11 20:21:26 +0000 2015');
+
+//console.log("TRACKING #therecameanecho")
+
 
 var stream = Twit.stream('statuses/filter', { track: '#ghostselfie' })
 
 stream.on('tweet', function (tweet) {
-  console.log(tweet)
-  console.log(tweet.user.name)
+  //console.log(tweet)
+  //console.log(tweet.user.name)
+  LogWriter.writeUserAndTime(tweet.user.name, tweet.user.screen_name, tweet.created_at);
+
 })
-*/
+

f278d2bb5faae45fa508d78151d5f849f38c6413
Arjun Prakash <archieoi@gmail.com>
2014-07-15 02:35:49 -0700

    socket io working

--- index.js
+++ index.js
@@ -1,35 +1,75 @@
-var fs = require('fs');
-var path = require('path');
-var twit = require('twit')
-var request = require('request');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var fs = require('fs')
+	, path = require('path')
+	, twit = require('twit')
+	, request = require('request');
+
 
 var config1 = require('twit/config1')
 
 var logwriter = require('./lib/logwriter')
 
-// Inits
 var Twit = new twit(config1);
+var username = null;
 
-var LogWriter = new logwriter('ghostLog.txt', function(exists) {
+var LogWriter = new logwriter('ghostLog.txt', function(exists) { });
 
+var stream = Twit.stream('statuses/filter', { track: '#ghostselfie' });
 
+// Testing webpage
+app.get('/ghostselfie', function(req, res){
+	res.sendfile('index.html');
+	/*
+	if(username !== null){
+		res.send(username);
+	} else {
+		res.send('nothing yet')
+	}
+	*/
 });
 
-//console.log(config1);
 
+// io connections
+io.on('connection', function(socket){
+	console.log('a user connected');
 
-//LogWriter.writeUserAndTime('mikel jordan','funkeybutt', 'Fri Jul 11 20:21:26 +0000 2014');
-//LogWriter.writeUserAndTime('rank lombard','rockandroller', 'Fri Jul 11 20:21:26 +0000 2015');
+	// sends out if user is tweeting
+	socket.on('get user', function(msg){
+		if(username !== null){
+			msg = username;
+		} else {
+			msg = "null";
+		}
+		console.log('sending user: ' + username);
+	   io.emit('sending user', msg);
+	});
 
-//console.log("TRACKING #therecameanecho")
+	// sends file that is ready and clears users
+	socket.on('file ready', function(msg){
+		
+		console.log('ready file: ' + msg);
+		//upload file here
+		username = null;
+	});
+	
+	// disconnect from server
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
+});
 
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
 
-var stream = Twit.stream('statuses/filter', { track: '#ghostselfie' })
-
+// twitter listning stream
 stream.on('tweet', function (tweet) {
   //console.log(tweet)
   //console.log(tweet.user.name)
+  username = tweet.user.name;
   LogWriter.writeUserAndTime(tweet.user.name, tweet.user.screen_name, tweet.created_at);
 
-})
-
+});
